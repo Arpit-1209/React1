35c512e4713fa61173f881a64257c2ac7a0dbd63
Arpit Srivastava <112778916+Arpit-1209@users.noreply.github.com>
2023-10-15 12:48:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import WordCounter from './components/WordCounter';
-import "C:/Users/Arpit Srivastava/OneDrive/Desktop/MERN fullstack/node_modules/bootstrap/dist/css/bootstrap.min.css";
+//import "C:/Users/Arpit Srivastava/OneDrive/Desktop/MERN fullstack/node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
-      <WordCounter/>      
+      <WordCounter/>  
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" ></link>   
     </div>
   );
 }
